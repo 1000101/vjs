689559cbb1c1204f8550f9e1d529343a7c3e308c
1000101 <b1000101@pm.me>
2021-04-14 22:10:30 +0200

    feat(001): add first lesson

---
+++ 001/script.js
@@ -0,0 +1,18 @@
+function myClick() {
+  alert("Hello");
+  //this throws Hello alert
+}
+
+// single line comment
+
+/*
+Conventions (how to name variables and functions):
+
+myClick myClickWhenSomethingHappens // camelCase <-- THIS
+
+
+my_click my_click_when_something_happens // snake_case
+
+MyClick MyClickWhenSomethingHappens // PascalCase
+myclick myclickwhensomethinghappens // NO-NO
+*/
