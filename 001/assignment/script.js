4a61cbf15f72a29ab63913a2ccab2b72706e6231
1000101 <b1000101@pm.me>
2021-04-14 22:20:32 +0200

    feat(readme): add assignment

---
+++ 001/assignment/script.js
@@ -0,0 +1,20 @@
+function myClick() {
+  /*
+
+  Adjust myClick() function to replace "Hello" with
+  the current date and time in format: d.m.yyyy hh:mm:ss
+  (e.g. 14.4.2021 16:20:00)
+
+  if you need to use variables, use either
+  let or const keywords, e.g.:
+
+  let time = "something";
+  const time = "something"
+
+
+  Hint: use standard JS Date() function
+  https://www.w3schools.com/jsref/jsref_obj_date.asp
+  */
+
+  alert("");
+}
--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Viki's JS Course
 
-001 - First JS function
+### 001 - First JS function
+
+Simple page that throws "Hello" alert on button click.
+
+Assignment: Adjust `myClick()` function to replace "Hello" with the current date and time in format: `d.m.yyyy hh:mm:ss` (e.g. `14.4.2021 16:20:00`). PRs (into `./001/assignment/`) are welcome!
